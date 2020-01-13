58c011003150bf143c229677b52b42e9f79f6a12
skamakshi-ds <43050969+skamakshi-ds@users.noreply.github.com>
2020-01-12 22:17:35 -0800

    Search Insert Position at given sorted array.
    Used to search the target data position at given sorted array or can be used to search what might be the anticipated position if the target is not included in the given array.
    e.g. : 
    i/p [1,3,5,6], 5
    o/p : 2 
    
    i/p : [1,3,5,6], 2
    o/p : 1
    
    i/p : [1,3,5,6], 7
    o/p : 4

--- testSearchSortedArray.java
+++ testSearchSortedArray.java
@@ -1,20 +1,31 @@
 package com.test.proj;
 
+/*
+* @author : CodeDock
+* @date : 01/04/2020
+* Used to search the target position at given sorted array or can 
+* be used to search what might be the anticipated position if the target
+* is not included in the given array.
+*/
+
 public class testSearchSortedArray {
 	public int searchInsert(int[] nums, int target) {
 		
 		int index = 0;
 		
+		// If the given target is smaller than or equal to the first element or 
 		if(target < nums[0] || target == nums[0]) {
 			return 0;
 		}
 		
+		// If the target present among the array elements
 		for(int i = 1; i < nums.length ; i++) {
 			if(target == nums[i] || (target > nums[i-1] && target < nums[i])) {
 					return i;
 				}
 		}
 		
+		// if the target is greater than the array element.
 		if(target > nums[nums.length-1])
 			return nums.length;
 		
